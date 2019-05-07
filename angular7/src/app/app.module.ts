ab3f67e9c5c64027dc366ae955940f3ead6b4692
Abhinav Singh <abhinav.singh@trantorchd.com>
2019-05-07 14:14:08 +0530

    angular7-ionic update

--- angular7/src/app/app.module.ts
+++ angular7/src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+const routes : Routes = [
+  { path : '', component : HomeComponent },
+  { path : 'about', component : AboutComponent },
+  { path : 'contact', component : ContactComponent }
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
